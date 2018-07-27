d2abe4c55599d1c98e680713d39c5645d187325c
sharma0611 <sharma.shivam0611@gmail.com>
2018-07-27 18:00:54 -0400

    add initial state values for redux store and add provider to app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,46 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import configureStore from './Store/store'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const formData = {'initialDeposit': 1,
+                  'monthlyDeposit': 1,
+                  'interestRate': 4}
+
+const initialState = {
+	'monthlyData' : [
+              {
+                month: 1,
+                amount: 500
+              },
+              {
+                month: 2,
+                amount: 700
+              },
+              {
+                month: 3,
+                amount: 1000
+              },
+              {
+                month: 4,
+                amount: 1500
+              }
+            ],
+  'hasErrored': false,
+  'isLoading': false,
+  formData
+};
+
+const store = configureStore(initialState);
+
+console.log(store)
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
 registerServiceWorker();
+
+
