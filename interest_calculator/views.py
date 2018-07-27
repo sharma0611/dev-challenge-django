26f8cac016ee08a165af7775ca64a95424d2573b
sharma0611 <sharma.shivam0611@gmail.com>
2018-07-27 18:06:31 -0400

    update api logic to calculate monthly data

--- interest_calculator/views.py
+++ interest_calculator/views.py
@@ -7,10 +7,27 @@ import json
 @csrf_exempt
 def calculate(request):
     params = json.loads(request.body)
-    savings_amount = params.get('savingsAmount', None)
-    interest_rate = params.get('interestRate', None)
+    monthly_deposit = params.get('monthlyDeposit', None)
+    initial_deposit = params.get('initialDeposit', None)
+    interest_rate = params.get('interestRate', None) 
+    if interest_rate:
+        interest_rate = 1 + float(interest_rate) / 100
+    result = []
 
-    if savings_amount is None or interest_rate is None:
-        return HttpResponseBadRequest('Required parameters are not provided')
+    apply_interest = 6 #every 6 mts
+    curr_balance = initial_deposit
+    for curr_month in range(50*12):
+        if curr_month % apply_interest == 0 and curr_month != 0: # interest applied at the start of the month
+            curr_balance = curr_balance * interest_rate
 
-    return JsonResponse({'result': 1000})
+        month_num = curr_month + 1
+        curr_result = {'month' : month_num,
+                'amount': curr_balance}
+
+        result.append(curr_result)
+        curr_balance += monthly_deposit #deposit made at the end of the month
+
+    #if savings_amount is None or interest_rate is None:
+    #    return HttpResponseBadRequest('Required parameters are not provided')
+
+    return JsonResponse({'result': result})
